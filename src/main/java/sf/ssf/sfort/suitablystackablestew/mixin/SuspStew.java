166b11cdbc42ea01b7100a1e8f0ef59b6b10c9ba
Derek Chen <derex1987@gmail.com>
2025-05-27 19:37:32 -0500

    feat: suspicious stew mixin was item componentized noooooooo

--- src/main/java/sf/ssf/sfort/suitablystackablestew/mixin/SuspStew.java
+++ src/main/java/sf/ssf/sfort/suitablystackablestew/mixin/SuspStew.java
@@ -1,21 +1,18 @@
 package sf.ssf.sfort.suitablystackablestew.mixin;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SuspiciousStewItem.class)
-public class SuspStew extends Item{
-	public SuspStew(Settings settings) {
-		super(settings);
-	}
-	@Inject(method="finishUsing", at=@At("TAIL"), cancellable = true)
-	private void use(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> info) {
+@Deprecated
+public class SuspStew extends Item {
+    public SuspStew(Settings settings) {
+        super(settings);
+    }
+
+    private void use(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> info) {
 //		info.setReturnValue(new ItemStack(Items.BOWL));
 //		if (!stack.isEmpty() && user instanceof PlayerEntity) {
 //			if (!((PlayerEntity) user).getAbilities().creativeMode) {
@@ -24,5 +21,5 @@ public class SuspStew extends Item{
 ////			info.setReturnValue(stack);
 //		}
 //		info.cancel();
-	}
+    }
 }
