ece61750aebf3df9a267e0fb906844c6499be165
Derek Chen <derex1987@gmail.com>
2025-06-01 16:57:19 -0500

    feat: 1.21.2 port

--- src/main/java/sf/ssf/sfort/suitablystackablestew/mixin/StewItemsMixin.java
+++
@@ -1,9 +0,0 @@
-package sf.ssf.sfort.suitablystackablestew.mixin;
-
-import net.minecraft.item.Items;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value = Items.class, priority = 1221)
-public class StewItemsMixin {
-
-}
